98e15ada3d3498139f39f6d6399193d6580d8625
Hector <hsandoval79@gmail.com>
2017-09-21 23:57:15 -0700

    update app add public views

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var express = require("express");
 
 var app= express();
 
-//app.use("view engine", "ejs");
+app.use("view engine", "ejs");
 
 app.get("/", function(req, res){
-	res.send("Hi there!");
+	res.render("index");
+});
+
+app.get("*", function(req, res){
+	res.send("You are a star!");
 });
 
 app.listen(3000,function(){
 	console.log("Local Server Started");
-})
+});
