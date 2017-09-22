3c704a26fc24b73016b7dfa9b1e6564635d58158
Hector <hsandoval79@gmail.com>
2017-09-22 00:28:37 -0700

    fix app.js index.ejs change main.css

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require("express");
 
 var app= express();
 
-//app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-	res.render("index.ejs");
+	res.render("index");
 });
 
 app.get("*", function(req, res){
